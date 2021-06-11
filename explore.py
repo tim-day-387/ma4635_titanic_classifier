8052f6e776b84149b58ae97cb4e699e0971845d6
Timothy Day <timothy@debian-desktop>
2021-06-10 23:20:17 -0400

    Updated explore to use functions

--- explore.py
+++ explore.py
@@ -1,36 +1,33 @@
 # Imports
 import pandas as pd
 
-# Data
-train = pd.read_csv("data/train.csv")
-total_passengers = len(train.Survived)
-total_survived = sum(train.Survived)
-total_percent = total_survived/total_passengers*100
+# Provides basics stats
+def giveStats(dataframe):
+    total_passengers = len(dataframe.Survived)
+    total_survived = sum(dataframe.Survived)
+    total_percent = total_survived/total_passengers*100
 
-print("Passengers: ", total_passengers, 
-      "\nSurvived: ", total_survived, 
-      "\nPercent: ", total_percent, "%")
-
+    print("------ Overview ------")
+    print("Passengers: ",total_passengers,"\nSurvived: ",total_survived,"\nPercent: ",total_percent,"%")
 
 # Explore Ticket
-query = 'SOTON'
-sel = []
-for i, row in train.iterrows():
-    if query in str(row.Ticket):
-        sel.append(i)
-sel_df = train.iloc[sel]
+def exploreTicket(dataframe,query):
+    sel = []
+    for i, row in dataframe.iterrows():
+        if query in str(row.Ticket):
+            sel.append(i)
+    sel_df = dataframe.iloc[sel]
 
-sel_passengers = len(sel_df.Survived)
-sel_survived = sum(sel_df.Survived)
-sel_percent = sel_survived/sel_passengers*100
+    sel_passengers = len(sel_df.Survived)
+    sel_survived = sum(sel_df.Survived)
+    if(sel_passengers != 0):
+        sel_percent = sel_survived/sel_passengers*100
+    else:
+        sel_percent = -1
 
-print("Passengers: ", sel_passengers, 
-      "\nSurvived: ", sel_survived, 
-      "\nPercent: ", sel_percent, "%")
+    print("------ Overview ------")
+    print("Passengers: ",sel_passengers,"\nSurvived: ",sel_survived,"\nPercent: ",sel_percent,"%")
 
-print("Ports:",
-     "\nC: ", len(sel_df.loc[sel_df.Embarked == 'C']),
-     "\nQ: ", len(sel_df.loc[sel_df.Embarked == 'Q']),
-     "\nS: ", len(sel_df.loc[sel_df.Embarked == 'S']),
-     )
+    print("-------- Ports -------")
+    print("C: ",len(sel_df.loc[sel_df.Embarked == 'C']),"\nQ: ",len(sel_df.loc[sel_df.Embarked == 'Q']),"\nS: ",len(sel_df.loc[sel_df.Embarked == 'S']),)
 
